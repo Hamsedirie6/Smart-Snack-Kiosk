a7c5e5f186eece68b84ff9af8c690636e1758ad7
gabrielozbek <gabriello14_@hotmail.com>
2026-03-24 12:16:29 +0100

    skapat modeller

---
+++ src/backend/SmartSnackKiosk.Api/Entities/Category.cs
@@ -0,0 +1,12 @@
+namespace SmartSnackKiosk.Api.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
