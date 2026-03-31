a2cf26c1cf98c19f028a857b51c8ce525056a360
hamsedirie6 <hamse_03@hotmail.se>
2026-03-31 12:09:45 +0200

    Lagt till validering på CreateSaleItemDto och LoginRequestDto

--- src/backend/SmartSnackKiosk.Api/DTOs/Auth/LoginrequestDto.cs
+++ src/backend/SmartSnackKiosk.Api/DTOs/Auth/LoginrequestDto.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SmartSnackKiosk.Api.DTOs.Auth;
 
 public class LoginRequestDto
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
 }
